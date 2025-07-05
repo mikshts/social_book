2596cfb79141da98ea2510446b1e14534cc34360
mikshts <villacampamichael123@gmail.com>
2025-07-05 15:55:16 +0800

    daphne bai

--- social_book/run_daphne.py
+++ social_book/run_daphne.py
@@ -7,7 +7,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_book.settings")
 
 CommandLineInterface().run([
     "-b", "0.0.0.0",
-    "-p", "8000",
-    "--",  # <- important: separates options from the application path
+    "-p", os.environ.get("PORT", "8000"),  # ✅ use Render's assigned port
+    "--",
     "social_book.asgi:application"
 ])
